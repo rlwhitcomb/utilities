a64dd00785e11f57ab7a93e04cd430486e66a73e
Roger Whitcomb <roger@whitcomb.info>
2020-09-22 14:45:00 -0700

    Bring "matches.bat" up to the same code as the other .bat wrapper files.

--- java/matches.bat
+++ java/matches.bat
@@ -1,1 +1,7 @@
-@java -cp %~dp0utilities.jar info.rlwhitcomb.matches.Matches %*
+@echo off
+setlocal
+set UTIL_JAR=%~dp0utilities.jar
+if not exist %UTIL_JAR% echo Unable to locate the required "utilities.jar" file in the "%~dp0" directory.&exit /b 1
+java -cp %UTIL_JAR% info.rlwhitcomb.matches.Matches %*
+endlocal
+
