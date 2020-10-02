087e0673ce9a279253e861221dcbb2ed3ff7ae32
Roger Whitcomb <roger@whitcomb.info>
2020-10-02 14:57:04 -0700

    Add Lists to the .jar file; add wrapper scripts for it.

---
+++ java/lists.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+if exist Lists*.class java Lists %*&exit /b %errorlevel%
+
+set UTIL_JAR=%~dp0utilities.jar
+if not exist %UTIL_JAR% echo Unable to locate the required "utilities.jar" file in the "%~dp0" directory.&exit /b 1
+java -cp %UTIL_JAR% Lists %*
+endlocal
+
