c33fecb7b47f88dbeca0010b70477b07333ca62a
Roger Whitcomb <roger.whitcomb@actian.com>
2020-10-21 18:09:24 -0700

    Code cleanup in OS.java: - Do matching of option names inside Choice enum - Add more Javadoc - Add help display

--- java/OS.java
+++ java/OS.java
@@ -33,7 +33,12 @@
  *    20-Oct-2020 (rlwhitcomb)
  *	Use streams and a FunctionalInterface to make the code cleaner.
  *	Add some Javadoc. Add the list of security providers.
+ *    21-Oct-2020 (rlwhitcomb)
+ *	More code cleanup; move the command line choice matching into
+ *	the Choice enum itself; display all the choices for the "help",
+ *	and implement a "help" option.
  */
+import java.io.PrintStream;
 import java.nio.charset.Charset;
 import java.security.Provider;
 import java.security.Security;
@@ -53,30 +58,65 @@ import java.util.TreeSet;
 public class OS
 {
 	/** Banner for the top of each report. */
-	private static final String HEADER = "=======================================";
+	private static final String HEADER = "=========================================";
 	/** Final banner for the end of the reports. */
-	private static final String FOOTER = "---------------------------------------";
+	private static final String FOOTER = "-----------------------------------------";
 
 	/**
-	 * What the user would like to display, and the method to display each bit.
+	 * What the user would like to display, the method to display each bit, and
+	 * the possible names to match.
 	 */
 	private enum Choice
 	{
-		PROPERTIES	(OS::displayProperties),
-		ENVIRONMENT	(OS::displayEnvironment),
-		CHARSETS	(OS::displayCharsets),
-		LOCALES		(OS::displayLocales),
-		DIGESTS		(OS::displayDigests),
-		PROVIDERS	(OS::displayProviders);
+		PROPERTIES	(OS::displayProperties,
+				 "properties", "props", "p"),
+		ENVIRONMENT	(OS::displayEnvironment,
+				 "environment", "environ", "env", "e"),
+		CHARSETS	(OS::displayCharsets,
+				 "charsets", "charset", "chars", "char", "ch", "c"),
+		LOCALES		(OS::displayLocales,
+				 "locales", "locale", "loc", "l"),
+		DIGESTS		(OS::displayDigests,
+				 "message-digests", "message_digests", "messagedigests",
+				 "digests", "digest", "dig", "d", "m", "md"),
+		PROVIDERS	(OS::displayProviders,
+				 "security-providers", "security_providers",
+				 "securityproviders", "providers", "security", "prov",
+				 "sec", "s", "sp");
 
 		private Runnable displayer;
+		private String[] aliasNames;
 
-		Choice(Runnable disp) {
-		    this.displayer = disp;
+		Choice(final Runnable disp, final String... names) {
+		    this.displayer  = disp;
+		    this.aliasNames = names;
 		}
 
 		void display() {
 		    displayer.run();
+		}
+
+		/**
+		 * Match the input against one of us.
+		 *
+		 * @param input	The input string to test.
+		 * @return	A matching choice or {@code null} if
+		 *		no match was made.
+		 */
+		static Choice match(final String input) {
+		    return Arrays.stream(values())
+				 .filter(c -> matches(input, c.aliasNames))
+				 .findFirst()
+				 .orElse(null);
+		}
+
+		/**
+		 * Display all the choices available for each choice type.
+		 */
+		static void displayAliases(final PrintStream ps, final String indent, final int width) {
+		    Arrays.stream(values())
+				.forEach(
+			c -> ps.format("%1$s,%n", makeArrayString(c.aliasNames, indent, width)));
 		}
 	}
 
@@ -85,6 +125,27 @@ public class OS
 	 */
 	private static Set<Choice> choices = EnumSet.noneOf(Choice.class);
 
+
+	private static String makeArrayString(final String[] values, final String indent, final int width) {
+	    int indentWidth = indent.length();
+	    int printWidth  = indentWidth;
+	    StringBuilder output = new StringBuilder(indent);
+
+	    for(String value : values) {
+		if (printWidth != indentWidth) {
+		    output.append(", ");
+		    printWidth += 2;
+		}
+		if (printWidth > width) {
+		    output.append('\n').append(indent);
+		    printWidth = indentWidth;
+		}
+		output.append('"').append(value).append('"');
+		printWidth += value.length() + 2;
+	    }
+
+	    return output.toString();
+	}
 
 	/**
 	 * Print a nicely formatted banner before the report, with the
@@ -111,6 +172,14 @@ public class OS
 	}
 
 	/**
+	 * Display the standard report footer.
+	 */
+	private static void printFooter() {
+	    System.out.println(FOOTER);
+	    System.out.println();
+	}
+
+	/**
 	 * Match an input string (presumably a command-line argument) with a list
 	 * of choices (aliases for a report choice), and test to see if any match.
 	 * <p> Note: the comparison is done ignoring letter case differences using
@@ -125,44 +194,46 @@ public class OS
 	}
 
 	/**
+	 * Print a "usage" or "help" message to the print stream (either {@link System#out}
+	 * or {@link System#err}).
+	 *
+	 * @param ps	The stream to output to.
+	 */
+	private static void usage(final PrintStream ps) {
+	    ps.println("Usage: java OS [choice]*");
+	    ps.println("Valid choices are:");
+	    Choice.displayAliases(ps, "    ", 60);
+	    ps.println(" or \"all\" (default is \"properties\"),");
+	    ps.println(" or \"help\", \"h\", or \"?\" to display this message.");
+	}
+
+	/**
 	 * Loop through the given command line arguments, determining which reports
 	 * have been selected to be run.
 	 *
 	 * @param args	The command line argument array.
 	 * @return	Whether or not the matching succeeded (meaning all of the args
-	 *		matchied one of our choices); {@code true} means success,
+	 *		matched one of our choices); {@code true} means success,
 	 *		while {@code false} indicates an error (the arg did not match
 	 *		ANY of the choices).
 	 */
 	private static boolean parseArgs(final String[] args) {
 	    for (String arg: args) {
-		if (matches(arg, "properties", "props", "p")) {
-		    choices.add(Choice.PROPERTIES);
-		}
-		else if (matches(arg, "environment", "environ", "env", "e")) {
-		    choices.add(Choice.ENVIRONMENT);
-		}
-		else if (matches(arg, "charsets", "charset", "chars", "char", "ch", "c")) {
-		    choices.add(Choice.CHARSETS);
-		}
-		else if (matches(arg, "locales", "locale", "loc", "l")) {
-		    choices.add(Choice.LOCALES);
-		}
-		else if (matches(arg, "message-digests", "message_digests", "messagedigests",
-				"digests", "digest", "dig", "d", "m", "md")) {
-		    choices.add(Choice.DIGESTS);
-		}
-		else if (matches(arg, "security-providers", "security_providers",
-				"securityproviders", "providers", "security", "prov", "sec", "s", "sp")) {
-		    choices.add(Choice.PROVIDERS);
+		Choice choice = Choice.match(arg);
+		if (choice != null) {
+		    choices.add(choice);
 		}
 		else if (matches(arg, "all", "a")) {
 		    choices = EnumSet.allOf(Choice.class);
 		}
+		else if (matches(arg, "help", "h", "?")) {
+		    usage(System.out);
+		    return false;
+		}
 		else {
 		    System.err.println("Unknown choice value of \"" + arg + "\".");
-		    System.err.println("Valid choices are: 'properties', 'environment', 'charsets', 'locales', 'digests', or 'all'.");
-		    System.err.println("  (default is 'properties')");
+		    System.err.println();
+		    usage(System.err);
 		    return false;
 		}
 	    }
@@ -183,8 +254,7 @@ public class OS
 		String value = sysProperties.getProperty(propertyName);
 		System.out.format("%1$s = %2$s%n", propertyName, value);
 	    }
-	    System.out.println(FOOTER);
-	    System.out.println();
+	    printFooter();
 	}
 
 	/**
@@ -197,8 +267,7 @@ public class OS
 	    for (Map.Entry<String, String> entry : env.entrySet()) {
 		System.out.format("%1$s = %2$s%n", entry.getKey(), entry.getValue());
 	    }
-	    System.out.println(FOOTER);
-	    System.out.println();
+	    printFooter();
 	}
 
 	/**
@@ -214,8 +283,7 @@ public class OS
 		String prefix = (entry.getValue().equals(defaultCharset)) ? "* " : "  ";
 		System.out.println(prefix + entry.getKey());
 	    }
-	    System.out.println(FOOTER);
-	    System.out.println();
+	    printFooter();
 	}
 
 	/**
@@ -238,8 +306,7 @@ public class OS
 		String prefix = (loc.equals(defaultLocale)) ? "*" : "";
 		System.out.format("%1$1s%2$15s  %3$s%n", prefix, tag, loc.getDisplayName());
 	    }
-	    System.out.println(FOOTER);
-	    System.out.println();
+	    printFooter();
 	}
 
 	/**
@@ -251,11 +318,20 @@ public class OS
 
 	    printTitle("Message Digests");
 	    sortedDigests.forEach(System.out::println);
-	    System.out.println(FOOTER);
-	    System.out.println();
+	    printFooter();
 	}
 
-	private static int compare(Provider p1, Provider p2) {
+	/**
+	 * A comparator of {@link Provider} objects, sorting by name.
+	 *
+	 * @param p1	The first object to compare.
+	 * @param p2	The second object to compare to the first.
+	 * @return	&lt; 0 if the name of the first is less than the
+	 *		name of the second, = 0 if the names are equal,
+	 *		and &gt; 0 if the name of the first is greater
+	 *		than the name of the second.
+	 */
+	private static int compareProviders(Provider p1, Provider p2) {
 	    return p1.getName().compareTo(p2.getName());
 	}
 
@@ -264,12 +340,11 @@ public class OS
 	 */
 	private static void displayProviders() {
 	    Provider[] providers = Security.getProviders();
-	    Arrays.sort(providers, OS::compare);
+	    Arrays.sort(providers, OS::compareProviders);
 
 	    printTitle("Security Providers");
 	    Arrays.stream(providers).forEach(p -> System.out.format("%1$12s: %2$s%n", p.getName(), p.getInfo()));
-	    System.out.println(FOOTER);
-	    System.out.println();
+	    printFooter();
 	}
 
 	/**
