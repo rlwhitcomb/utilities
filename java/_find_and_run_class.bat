b78b791cb8768a3807c6e7c58ae921c89286fef8
Roger Whitcomb <roger.whitcomb@actian.com>
2020-11-24 23:05:32 -0800

    Copy the utility wrapper files to the UTILITIES_HOME directory along with the .jar file. Set the UTILITIES_HOME in the standard environment. Add the base wrapper script.

---
+++ java/_find_and_run_class.bat
@@ -0,0 +1,5 @@
+@echo off
+if "%JAR_FILE%" == "" set JAR_FILE=utilities.jar
+set JAR_PATH=%~dp0%JAR_FILE%
+if not exist %JAR_PATH% echo Unable to locate the required "%JAR_FILE%" file in the "%~dp0" directory.&exit /b 1
+java -cp %JAR_PATH% %CLASS_NAME% %*
