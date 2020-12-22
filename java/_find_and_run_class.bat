7c67df253aa327c12e2490b31b7454e712e514e3
Roger Whitcomb <roger@whitcomb.info>
2020-12-22 09:35:26 -0800

    Derive all the build values in Environment directly from the properties files now included in the .jar file.

--- java/_find_and_run_class.bat
+++ java/_find_and_run_class.bat
@@ -2,4 +2,4 @@
 if "%JAR_FILE%" == "" set JAR_FILE=utilities.jar
 set JAR_PATH=%~dp0%JAR_FILE%
 if not exist %JAR_PATH% echo Unable to locate the required "%JAR_FILE%" file in the "%~dp0" directory.&exit /b 1
-java -cp %CLASSPATH%;%JAR_PATH% %CLASS_NAME% %*
+java -cp %JAR_PATH%;%CLASSPATH% %CLASS_NAME% %*
