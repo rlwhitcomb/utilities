966d7772046030bda287479863fa11759427b465
Roger Whitcomb <roger.whitcomb@actian.com>
2021-01-11 12:26:27 -0800

    Add some missing "package-info" files.

---
+++ java/info/rlwhitcomb/curl/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * A GUI interface for the "curl" utility; written using the Apache Pivot&trade; framework.
+ */
+package info.rlwhitcomb.curl;
+
---
+++ java/info/rlwhitcomb/wordfind/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * A word finding program that uses a dictionary to find all the valid words from a given combination of letters.
+ */
+package info.rlwhitcomb.wordfind;
+
