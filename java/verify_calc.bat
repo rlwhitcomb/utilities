20b5725ca5cf3508d53a5fcb75e26b9419af705f
Roger Whitcomb <roger@whitcomb.info>
2021-04-12 10:50:42 -0700

    Fix bugs in "verify_calc.bat" process.

--- java/verify_calc.bat
+++ java/verify_calc.bat
@@ -2,7 +2,7 @@
 :: Verify the calculation of Mersenne prime M43112609
 set NUMBER=43112609
 set DIGITS=12978189
-if exists test\data\M%NUMBER%.txt.gz (copy test\data\M%NUMBER%.txt.gz .) && (call gunz M%NUMBER%.txt.gz)
+if exist test\data\M%NUMBER%.txt.gz (copy test\data\M%NUMBER%.txt.gz .) && (call gunz M%NUMBER%.txt.gz)
 call lists -single M%NUMBER%.txt >ref.txt
 call c -nocolor -r ":unl;(2**%NUMBER%)-1" >calc.txt
 for /f %%f in ('call c -nocolor verify') do set count=%%f
