587d77cd8c5c36a97b24bdcfa39cbf85fdf561e8
Roger Whitcomb <roger@whitcomb.info>
2021-04-22 11:03:17 -0700

    Add package info for the two newest packages.

---
+++ java/info/rlwhitcomb/test/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package containing the majority of our test programs.
+ */
+package info.rlwhitcomb.test;
+
---
+++ java/info/rlwhitcomb/tools/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Java implementations of useful command-line tools (some emulate *nix
+ * utilities missing on Windows)
+ */
+package info.rlwhitcomb.tools;
+
