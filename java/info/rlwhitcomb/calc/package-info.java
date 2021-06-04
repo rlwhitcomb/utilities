643b1b484e63adaf8424a6c027fb76eba0eb0354
Roger Whitcomb <roger.whitcomb@actian.com>
2021-06-04 08:14:56 -0700

    Add the Calc help page to the Javadoc for reference. Fix some problems with the help page.

--- java/info/rlwhitcomb/calc/package-info.java
+++ java/info/rlwhitcomb/calc/package-info.java
@@ -1,5 +1,6 @@
 /**
  * An expression calculator with (essentially) unlimited precision; built using the Antlr parser generator.
+ * <p> The complete help page can be found <a href="../../../doc-files/calc_help.html">here</a>.
  */
 package info.rlwhitcomb.calc;
 
