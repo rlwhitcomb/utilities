9c9845961f6341516a638cfb73264de3b67bc49c
rlwhitcomb <roger@whitcomb.info>
2021-08-04 16:32:47 -0700

    Fix two "package-info" files.

--- java/de/onyxbits/package-info.java
+++ java/de/onyxbits/package-info.java
@@ -2,7 +2,7 @@
  * A Semantic Version parser sourced from:
  * <a href="https://raccoon.onyxbits.de/blog/single-class-java-semantic-versioning-parser-implementation/">
 https://raccoon.onyxbits.de/blog/single-class-java-semantic-versioning-parser-implementation/</a>
- * with some addiions and corrections, and assigned a package.
+ * with some additions and corrections, and assigned a package.
  */
 package de.onyxbits;
 
--- java/net/iharder/b64/package-info.java
+++ java/net/iharder/b64/package-info.java
@@ -1,7 +1,8 @@
 /**
- * A Base64 utility class sourced from here: http://iharder.sourceforge.net/current/java/base64/
+ * A Base64 utility class sourced from here:
+<a href="http://iharder.sourceforge.net/current/java/base64/">http://iharder.sourceforge.net/current/java/base64/</a>
  * with modifications of our own.
- * <p> Note: this is Public Domain code; no license necessary.
+ * <p> Note: this is Public Domain code.
  */
 package net.iharder.b64;
 
