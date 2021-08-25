bab96e61151351dda2506243777427c6bda14374
rlwhitcomb <roger@whitcomb.info>
2021-08-25 16:49:30 -0700

    Slightly different debugging messages in CI build script.

--- ci-build.sh
+++ ci-build.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 . ci-setenv
+echo Start of "ci-build.sh", current directory: `pwd`
 cd /home/runner/work/utilities/utilities/java
-echo Current directory listing
-echo -------------------------
-ls -al
-echo Current environment
-echo -------------------
-env | grep -i HOME
-echo
 ant -DCI_BUILD=true clean update doc test
 if [[ $? -ne 0 ]]
 then
    cat dist/test*.log
    exit 1
 fi
-echo Distribution Files
-echo ------------------
-ls -al dist
