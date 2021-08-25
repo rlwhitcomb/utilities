1caae0221fc60a5ff58f71156dfce99f849b0ce3
rlwhitcomb <roger@whitcomb.info>
2021-08-25 16:16:00 -0700

    Some more printouts in the CI build file to debug uploads.

--- ci-build.sh
+++ ci-build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 . ci-setenv
 cd /home/runner/work/utilities/utilities/java
-ls
+echo Current directory listing
+echo -------------------------
+ls -al
+echo Current environment
+echo -------------------
+env | grep -i HOME
+echo
 ant -DCI_BUILD=true clean update doc test
 if [[ $? -ne 0 ]]
 then
