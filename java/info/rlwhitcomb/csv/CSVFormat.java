75c6a9213e9fb6ea6036d1db81f2bcbbddb5128b
rlwhitcomb <roger@whitcomb.info>
2021-12-18 15:32:00 -0800

    #146: Refactor CSVFormat (additional methods to expose properties).

--- java/info/rlwhitcomb/csv/CSVFormat.java
+++ java/info/rlwhitcomb/csv/CSVFormat.java
@@ -53,9 +53,16 @@
  *	17-Feb-2021 (rlwhitcomb)
  *	    Add new "hasHeaderRow" and "preserveQuotes" flags; new
  *	    "getLeftQuote" and "getRightQuote" methods.
+ *	15-Dec=2021 (rlwhitcomb)
+ *	    #146: Additional methods to expose new flags and such.
+ *	    Okay, okay, this isn't a true Java Bean because the get/set methods
+ *	    are not symmetric (partly due to the "Builder" pattern and partly
+ *	    because the flag names aren't quite grammatical with "is" prefix).
+ *	    Add "Scriptable" annotations for fields to be exposed.
  */
 package info.rlwhitcomb.csv;
 
+import info.rlwhitcomb.annotations.Scriptable;
 import info.rlwhitcomb.util.Intl;
 
 
@@ -84,30 +91,37 @@ public class CSVFormat
 	public static final char DEFAULT_ESCAPE_CHAR = '\\';
 
 	/** Our custom quote character. */
+	@Scriptable
 	protected char quoteChar = DEFAULT_QUOTE_CHAR;
 
 	/** Our custom right-quote character (if different than the single one). */
+	@Scriptable
 	protected char rightQuoteChar = DEFAULT_QUOTE_CHAR;
 
 	/** Flag for easy testing to see if the left and right quotes are different. */
 	protected boolean handedQuoting = false;
 
 	/** Our custom field separator character. */
+	@Scriptable
 	protected char fieldSepChar = DEFAULT_FIELD_SEP_CHAR;
 
 	/** Our custom record separator string. */
+	@Scriptable
 	protected String recordSep = DEFAULT_RECORD_SEP;
 
+	/** For one character record separator, that character. */
+	protected char recordSepChar = '\n';
+
 	/** Our custom escape character. */
+	@Scriptable
 	protected char escapeChar = DEFAULT_ESCAPE_CHAR;
 
-	/** For one character record separator, that character. */
-	protected char recordSepChar = '\0';
-
 	/** Whether or not to preserve extra whitespace in unquoted tokens. */
+	@Scriptable
 	protected boolean preserveWhitespace = false;
 
 	/** Whether or not to ignore empty lines. */
+	@Scriptable
 	protected boolean ignoreEmptyLines = false;
 
 	/** Whether or not the line ending (record separator) is a "normal" value. */
@@ -120,12 +134,15 @@ public class CSVFormat
 	protected boolean disableQuoting = false;
 
 	/** Whether string values are always delimited on output (regardless of contents). */
+	@Scriptable
 	protected boolean alwaysDelimitStrings = false;
 
 	/** Whether to preserve quotes around values on input. */
+	@Scriptable
 	protected boolean preserveQuotes = false;
 
 	/** Whether on input there is a header row before the actual data. */
+	@Scriptable
 	protected boolean hasHeaderRow = false;
 
 
@@ -143,9 +160,9 @@ public class CSVFormat
 	 * @param	fieldSepChar	Field separator.
 	 * @param	recordSep	Separator for lines (records).
 	 */
-	public CSVFormat(char quoteChar, char fieldSepChar, String recordSep) {
+	public CSVFormat(final char quoteChar, final char fieldSepChar, final String recordSep) {
 	    this();
-	    this.withQuoteChar(quoteChar).withFieldSepChar(fieldSepChar).withRecordSep(recordSep);
+	    withQuoteChar(quoteChar).withFieldSepChar(fieldSepChar).withRecordSep(recordSep);
 	}
 
 	/**
@@ -153,9 +170,9 @@ public class CSVFormat
 	 *
 	 * @param	fieldSepChar	Custom field separator.
 	 */
-	public CSVFormat(char fieldSepChar) {
+	public CSVFormat(final char fieldSepChar) {
 	    this();
-	    this.withFieldSepChar(fieldSepChar);
+	    withFieldSepChar(fieldSepChar);
 	}
 
 	/**
@@ -164,20 +181,21 @@ public class CSVFormat
 	 * you intend to implement "handed quoting" (where the left and right quotes are different)
 	 * then set this first and the {@link #withRightQuoteChar} second.
 	 *
-	 * @param	quoteChar	The new quote character to use for formatting or parsing.  Can
-	 *				be {@code NUL} to disable quote recognition.
+	 * @param	newQuoteChar	The new quote character to use for formatting or parsing.  Can
+	 *				be {@code '\0'} or {@link Quotes.Constants#NO_QUOTE_CHAR}
+	 *				to disable quote recognition.
 	 *
 	 * @return	The {@link CSVFormat} object (updated).
 	 *
 	 * @throws	IllegalArgumentException if the quote character is the same as the escape character.
 	 */
-	public CSVFormat withQuoteChar(char quoteChar) {
-	    if (quoteChar == escapeChar)
+	public CSVFormat withQuoteChar(final char newQuoteChar) {
+	    if (newQuoteChar == escapeChar)
 		throw new Intl.IllegalArgumentException("csv#format.quoteNotEscape");
-	    this.quoteChar = this.rightQuoteChar = quoteChar;
-	    if (this.quoteChar == Quotes.Constants.NO_QUOTE_CHAR)
-		this.disableQuoting = true;
-	    this.handedQuoting = false;
+	    quoteChar = rightQuoteChar = newQuoteChar;
+	    if (quoteChar == Quotes.Constants.NO_QUOTE_CHAR)
+		disableQuoting = true;
+	    handedQuoting = false;
 	    return this;
 	}
 
@@ -186,23 +204,39 @@ public class CSVFormat
 	 * <p> This only sets the right quote character (and the internal {@link #handedQuoting} flag),
 	 * so call this method AFTER calling {@link #withQuoteChar}.
 	 *
-	 * @param	rightQuoteChar	The new right quote character to use for formatting or parsing
-	 *				(can be {@code '\0'} to disable handed quoting).
+	 * @param	newRightQuoteChar	The new right quote character to use for formatting or parsing
+	 *					(can be {@code '\0'} or {@link Quotes.Constants#NO_QUOTE_CHAR}
+	 *					to disable handed quoting).
 	 *
 	 * @return	The {@link CSVFormat} object (updated).
 	 *
 	 * @throws	IllegalArgumentException if the quote character is the same as the escape character.
 	 */
-	public CSVFormat withRightQuoteChar(char rightQuoteChar) {
-	    if (rightQuoteChar == escapeChar)
+	public CSVFormat withRightQuoteChar(final char newRightQuoteChar) {
+	    if (newRightQuoteChar == escapeChar)
 		throw new Intl.IllegalArgumentException("csv#format.quoteNotEscape");
-	    this.rightQuoteChar = rightQuoteChar == Quotes.Constants.NO_QUOTE_CHAR ? this.quoteChar : rightQuoteChar;
-	    this.handedQuoting = this.quoteChar != this.rightQuoteChar;
+	    rightQuoteChar = (newRightQuoteChar == Quotes.Constants.NO_QUOTE_CHAR ? quoteChar : newRightQuoteChar);
+	    handedQuoting = (quoteChar != rightQuoteChar);
 	    return this;
 	}
 
 	/**
-	 * Select the quote character(s) from the given enum value.
+	 * Set the left and right quote characters of the already existing format.
+	 *
+	 * @param	newQuoteChar		The new left quote character.
+	 * @param	newRightQuoteChar	The new right quote character.
+	 *
+	 * @return	The {@link CSVFormat} object (updated).
+	 *
+	 * @see #withQuoteChar
+	 * @see #withRightQuoteChar
+	 */
+	public CSVFormat withQuotes(final char newQuoteChar, final char newRightQuoteChar) {
+	    return withQuoteChar(newQuoteChar).withRightQuoteChar(newRightQuoteChar);
+	}
+
+	/**
+	 * Set the quote character(s) from the given enum value.
 	 *
 	 * @param	quote	The {@link Quotes} value that will determine the left (and possibly right)
 	 *			quote characters.
@@ -211,30 +245,25 @@ public class CSVFormat
 	 *
 	 * @throws	IllegalArgumentException if the quote value is {@code null}.
 	 */
-	public CSVFormat withQuote(Quotes quote) {
+	public CSVFormat withQuote(final Quotes quote) {
 	    if (quote == null)
 		throw new Intl.IllegalArgumentException("csv#format.quoteNotNull");
-	    this.quoteChar = quote.leftChar();
-	    this.rightQuoteChar = quote.rightChar();
-	    this.handedQuoting = this.quoteChar != this.rightQuoteChar;
-	    if (this.quoteChar == Quotes.Constants.NO_QUOTE_CHAR)
-		this.disableQuoting = true;
-	    return this;
+	    return withQuotes(quote.leftChar(), quote.rightChar());
 	}
 
 	/**
 	 * Set the field separator of an already existing format.
 	 *
-	 * @param	fieldSepChar	The new field separator to use for formatting and parsing.
+	 * @param	newFieldSepChar	The new field separator to use for formatting and parsing.
 	 *
 	 * @return	The {@link CSVFormat} object (updated).
 	 *
 	 * @throws	IllegalArgumentException if the separator character is a {@code NUL}.
 	 */
-	public CSVFormat withFieldSepChar(char fieldSepChar) {
-	    if (fieldSepChar == '\0')
+	public CSVFormat withFieldSepChar(final char newFieldSepChar) {
+	    if (newFieldSepChar == '\0')
 		throw new Intl.IllegalArgumentException("csv#format.fieldSepNotNull");
-	    this.fieldSepChar = fieldSepChar;
+	    fieldSepChar = newFieldSepChar;
 	    return this;
 	}
 
@@ -247,17 +276,41 @@ public class CSVFormat
 	 *
 	 * @throws	IllegalArgumentException if the enum value is {@code null}.
 	 */
-	public CSVFormat withDelimiter(Delimiter delimiter) {
+	public CSVFormat withDelimiter(final Delimiter delimiter) {
 	    if (delimiter == null)
 		throw new Intl.IllegalArgumentException("csv#format.fieldSepNotNull");
-	    this.fieldSepChar = delimiter.delim();
+	    fieldSepChar = delimiter.delim();
 	    return this;
+	}
+
+	/**
+	 * Decide if the given string is a "normal" line ending, which is a single CR, single LF,
+	 * CR, LF or LF, CR (non-standard, but whatever).
+	 *
+	 * @param sepString	Candidate record separator string.
+	 * @return		Whether or not it is "normal".
+	 */
+	private static boolean isNormal(final String sepString) {
+	    int len = sepString.length();
+	    if (len == 1 || len == 2) {
+		char ch1 = sepString.charAt(0);
+		if (len > 1) {
+		    char ch2 = sepString.charAt(1);
+		    return (ch1 == '\r' || ch1 == '\n') &&
+			   (ch2 == '\r' || ch2 == '\n') &&
+			   (ch1 != ch2);
+		}
+		else {
+		    return (ch1 == '\r' || ch1 == '\n');
+		}
+	    }
+	    return false;
 	}
 
 	/**
 	 * Set the record separator of an already existing format.
 	 *
-	 * @param	recordSep	{@code null} to set the default line ending
+	 * @param	newRecordSep	{@code null} to set the default line ending
 	 *				(see {@link #DEFAULT_RECORD_SEP}) or a
 	 *				one or two character string that will
 	 *				mark the end of a record.
@@ -268,32 +321,28 @@ public class CSVFormat
 	 *		or if the length is two and both characters are the same, or if the string
 	 *		does not represent a "normal" line ending and the length is greater than one.
 	 */
-	public CSVFormat withRecordSep(String recordSep) {
-	    if (recordSep == null || recordSep.isEmpty()) {
-		this.recordSep = DEFAULT_RECORD_SEP;
-		this.recordSepChar = '\n';
-		this.normalLineEnding = true;
+	public CSVFormat withRecordSep(final String newRecordSep) {
+	    if (newRecordSep == null || newRecordSep.isEmpty()) {
+		recordSep = DEFAULT_RECORD_SEP;
+		recordSepChar = '\n';
+		normalLineEnding = true;
 		return this;
 	    }
-	    else if (recordSep.length() > 2)
+	    else if (newRecordSep.length() > 2)
 		throw new Intl.IllegalArgumentException("csv#format.recordSepTwoChars");
-	    else if (recordSep.length() == 2) {
-		if (recordSep.charAt(0) == recordSep.charAt(1))
+	    else if (newRecordSep.length() == 2) {
+		if (newRecordSep.charAt(0) == newRecordSep.charAt(1))
 		    throw new Intl.IllegalArgumentException("csv#format.recordSepTwoDifferent");
 	    }
+
 	    // Decide if this is a "normal" line ending, which has special meaning during parsing
-	    this.normalLineEnding = true;
-	    for (int i = 0; i < recordSep.length(); i++) {
-		if (recordSep.charAt(i) != '\r' || recordSep.charAt(i) != '\n') {
-		    this.normalLineEnding = false;
-		    break;
-		}
-	    }
-	    if (!normalLineEnding && recordSep.length() > 1)
+	    normalLineEnding = isNormal(newRecordSep);
+
+	    if (!normalLineEnding && newRecordSep.length() > 1)
 		throw new Intl.IllegalArgumentException("csv#format.recordSepStandard");
 
-	    this.recordSep = recordSep;
-	    this.recordSepChar = recordSep.charAt(0);
+	    recordSep = newRecordSep;
+	    recordSepChar = recordSep.charAt(0);
 
 	    return this;
 	}
@@ -301,16 +350,12 @@ public class CSVFormat
 	/**
 	 * Set the record separator to the given character.
 	 *
-	 * @param	recordSepChar	The new value for the record separator character for formatting and parsing.
+	 * @param	newRecordSepChar	The new value for the record separator character for formatting and parsing.
 	 *
 	 * @return	The updated {@link CSVFormat} object.
 	 */
-	public CSVFormat withRecordSepChar(char recordSepChar) {
-	    this.recordSep = Character.toString(recordSepChar);
-	    this.recordSepChar = recordSepChar;
-	    if (recordSepChar == '\r' || recordSepChar == '\n')
-		this.normalLineEnding = true;
-	    return this;
+	public CSVFormat withRecordSepChar(final char newRecordSepChar) {
+	    return withRecordSep(Character.toString(newRecordSepChar));
 	}
 
 	/**
@@ -320,25 +365,25 @@ public class CSVFormat
 	 *
 	 * @return	The updated {@link CSVFormat} object.
 	 */
-	public CSVFormat withSeparator(Separator separator) {
+	public CSVFormat withSeparator(final Separator separator) {
 	    return withRecordSep(separator.getSeparator());
 	}
 
 	/**
 	 * Set the escape character of an already existing format.
 	 *
-	 * @param	escapeChar	The new value for the escape character for formatting and parsing.
+	 * @param	newEscapeChar	The new value for the escape character for formatting and parsing.
 	 *
 	 * @return	The updated {@link CSVFormat} object.
 	 *
 	 * @throws	IllegalArgumentException if this escape character is the same as the quote character.
 	 */
-	public CSVFormat withEscapeChar(char escapeChar) {
-	    if (escapeChar == quoteChar)
+	public CSVFormat withEscapeChar(final char newEscapeChar) {
+	    if (newEscapeChar == quoteChar)
 		throw new Intl.IllegalArgumentException("csv#format.escapeNotQuote");
-	    this.escapeChar = escapeChar;
+	    escapeChar = newEscapeChar;
 	    if (escapeChar == '\0')
-		this.disableEscape = true;
+		disableEscape = true;
 	    return this;
 	}
 
@@ -349,8 +394,8 @@ public class CSVFormat
 	 *
 	 * @return	The updated {@link CSVFormat} object.
 	 */
-	public CSVFormat withPreserveWhitespace(boolean flag) {
-	    this.preserveWhitespace = flag;
+	public CSVFormat withPreserveWhitespace(final boolean flag) {
+	    preserveWhitespace = flag;
 	    return this;
 	}
 
@@ -361,8 +406,8 @@ public class CSVFormat
 	 *
 	 * @return	The updated {@link CSVFormat} object.
 	 */
-	public CSVFormat withIgnoreEmptyLines(boolean flag) {
-	    this.ignoreEmptyLines = flag;
+	public CSVFormat withIgnoreEmptyLines(final boolean flag) {
+	    ignoreEmptyLines = flag;
 	    return this;
 	}
 
@@ -373,8 +418,8 @@ public class CSVFormat
 	 *
 	 * @return	The updated {@link CSVFormat} object.
 	 */
-	public CSVFormat withAlwaysDelimitStrings(boolean flag) {
-	    this.alwaysDelimitStrings = flag;
+	public CSVFormat withAlwaysDelimitStrings(final boolean flag) {
+	    alwaysDelimitStrings = flag;
 	    return this;
 	}
 
@@ -385,8 +430,8 @@ public class CSVFormat
 	 *
 	 * @return	The updated {@link CSVFormat} object.
 	 */
-	public CSVFormat withPreserveQuotes(boolean flag) {
-	    this.preserveQuotes = flag;
+	public CSVFormat withPreserveQuotes(final boolean flag) {
+	    preserveQuotes = flag;
 	    return this;
 	}
 
@@ -397,21 +442,20 @@ public class CSVFormat
 	 *
 	 * @return	The updated {@link CSVFormat} object.
 	 */
-	public CSVFormat withHasHeaderRow(boolean flag) {
-	    this.hasHeaderRow = flag;
+	public CSVFormat withHasHeaderRow(final boolean flag) {
+	    hasHeaderRow = flag;
 	    return this;
 	}
 
 	/**
-	 * Is this a "normal" line ending value, meaning is it any of the following:
-	 * <code>CR</code> or <code>LF</code> or <code>CRLF</code>
-	 * <p> This has special meaning in that any of these combinations will also
-	 * match any of these values in the parsed file.
+	 * Are there different quotes on the left and right sides of strings?
+	 * <p> This corresponds to several standard sets of {@code Quotes}, such as
+	 * {@link Quotes#SMARTSINGLE} and {@link Quotes#SMARTDOUBLE}.
 	 *
-	 * @return	The current value of the "normal line ending" flag.
+	 * @return	Whether or not the left and right quotes are different.
 	 */
-	protected boolean isNormalLineEnding() {
-	    return normalLineEnding;
+	protected boolean isHandedQuoting() {
+	    return handedQuoting;
 	}
 
 	/**
@@ -427,5 +471,90 @@ public class CSVFormat
 	protected char getRightQuote() {
 	    return rightQuoteChar;
 	}
+
+	/**
+	 * Are we preserving whitespace around non-quoted values?
+	 *
+	 * @return	The {@link #preserveWhitespace} flag.
+	 */
+	protected boolean isPreservingWhitespace() {
+	    return preserveWhitespace;
+	}
+
+	/**
+	 * Are we ignoring empty lines on input?
+	 *
+	 * @return	The {@link #ignoreEmptyLines} flag.
+	 */
+	protected boolean isIgnoringEmptyLines() {
+	    return ignoreEmptyLines;
+	}
+
+	/**
+	 * Is this a "normal" line ending value, meaning is it any of the following:
+	 * <code>CR</code> or <code>LF</code> or <code>CRLF</code>
+	 * <p> This has special meaning in that any of these combinations will also
+	 * match any of these values in the parsed file.
+	 *
+	 * @return	The current value of the "normal line ending" flag.
+	 */
+	protected boolean isNormalLineEnding() {
+	    return normalLineEnding;
+	}
+
+	/**
+	 * Is escaping disabled by the lack of an escape character? Set when the escape
+	 * character is set to {@code '\0'}.
+	 *
+	 * @return	The {@link #disableEscape} flag.
+	 */
+	protected boolean isEscapeDisabled() {
+	    return disableEscape;
+	}
+
+	/**
+	 * Is quoting disabled by the quote characters being set to {@code '\0'}.
+	 *
+	 * @return	The {@link #disableQuoting} flag.
+	 */
+	protected boolean isQuotingDisabled() {
+	    return disableQuoting;
+	}
+
+	/**
+	 * Are we always delimiting strings on output? Normally strings don't need to be
+	 * delimited unless they contain separators and/or leading or trailing spaces, but
+	 * some programs basically differentiate between numbers and strings by having
+	 * strings always quoted. So, that's what this option is used for.
+	 *
+	 * @return	The {@link #alwaysDelimitStrings} flag.
+	 */
+	protected boolean isAlwaysDelimitingStrings() {
+	    return alwaysDelimitStrings;
+	}
+
+	/**
+	 * Are we preserving quotes on input? Normally quotes around input fields are discarded
+	 * on input, leaving the pure values, but some programs distinguish between numeric and
+	 * string values by the quotes around strings, so preserving them will help to distinguish
+	 * data types on input.
+	 *
+	 * @return	The {@link #preserveQuotes} flag.
+	 */
+	protected boolean isPreservingQuotes() {
+	    return preserveQuotes;
+	}
+
+	/**
+	 * Does the data have a header row? This affects both input and output. On input the header
+	 * row provides column/field names, while on output IF headers have been provided they will
+	 * be used, otherwise generic names will be used.
+	 *
+	 * @return	The {@link #hasHeaderRow} flag.
+	 */
+	protected boolean isHasHeaderRow() {
+	    return hasHeaderRow;
+	}
+
 }
 
