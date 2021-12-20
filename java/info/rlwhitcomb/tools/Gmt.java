5a8325aaf5c23530ec38bfb039d6dc91685ff6d1
rlwhitcomb <roger@whitcomb.info>
2021-12-19 22:50:30 -0800

    #158: Add "-iso" (ISO-8601) format to Gmt; add help.

--- java/info/rlwhitcomb/tools/Gmt.java
+++ java/info/rlwhitcomb/tools/Gmt.java
@@ -32,6 +32,8 @@
  *	    Tweak the output so it matches what we're emulating better.
  *	29-Mar-2021 (rlwhitcomb)
  *	    Move to new package.
+ *	19-Dec-2021 (rlwhitcomb)
+ *	    #158: Add ISO-8601 format and help.
  */
 package info.rlwhitcomb.tools;
 
@@ -52,6 +54,28 @@ public class Gmt
 	private static final String DEFAULT_FORMAT = "E MMM dd,yyyy HH:mm:ss.SSS z";
 	/** Output format compatible with our {@code Logging} class. */
 	private static final String LOGGING_FORMAT = "MMM dd,yyyy HH:mm:ss.SSS z";
+	/** Output format compatible with ISO-8601 format. */
+	private static final String ISO_8601_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+
+	/** The help text for this program. */
+	private static final String[] HELP = {
+	    "",
+	    "Usage: gmt [options]",
+	    "",
+	    "\twhere 'options' can be:",
+	    "\t  -log  = display in format compatible with our Logging class",
+	    "\t  -date = display compatible with Linux 'date' command",
+	    "\t  -iso  = display in ISO-8601 format",
+	    "",
+	    "\tNote: options can be with or without '-' or can be just the first character.",
+	    "",
+	    "\tExamples:",
+	    "\t  -log  -> Dec 20,2021 06:45:49.829 GMT",
+	    "\t  -date -> Mon Dec 20  6:46:19 GMT 2021",
+	    "\t  -iso  -> 2021-12-20T06:46:43Z",
+	    "\tdefault -> Mon Dec 20,2021  6:48:22.584 GMT",
+	    ""
+	};
 
 
 	private static void sub(StringBuffer buf, int charPos) {
@@ -68,16 +92,36 @@ public class Gmt
 	    if (args.length > 0) {
 		String format = args[0];
 
-		if (format.equalsIgnoreCase("-log")
-		 || format.equalsIgnoreCase("log")
-		 || format.equalsIgnoreCase("-l")
-		 || format.equalsIgnoreCase("l"))
-		    dateFormat = LOGGING_FORMAT;
-		else if (format.equalsIgnoreCase("-date")
-		      || format.equalsIgnoreCase("date")
-		      || format.equalsIgnoreCase("-d")
-		      || format.equalsIgnoreCase("d"))
-		    dateFormat = DATE_FORMAT;
+		switch (format.toLowerCase()) {
+		    case "-log":
+		    case "log":
+		    case "-l":
+		    case "l":
+			dateFormat = LOGGING_FORMAT;
+			break;
+		    case "-date":
+		    case "date":
+		    case "-d":
+		    case "d":
+			dateFormat = DATE_FORMAT;
+			break;
+		    case "-iso":
+		    case "iso":
+		    case "-i":
+		    case "i":
+			dateFormat = ISO_8601_FORMAT;
+			break;
+		    case "-help":
+		    case "help":
+		    case "-h":
+		    case "h":
+		    case "-?":
+		    case "?":
+			for (String helpLine : HELP) {
+			    System.out.println(helpLine);
+			}
+			return;
+		}
 	    }
 
 	    SimpleDateFormat fmt = new SimpleDateFormat(dateFormat);
@@ -100,6 +144,10 @@ public class Gmt
 		case DEFAULT_FORMAT:
 		    sub(buf, 8);
 		    sub(buf, 16);
+		    break;
+		case ISO_8601_FORMAT:
+		    sub(buf, 5);
+		    sub(buf, 10);
 		    break;
 	    }
 
