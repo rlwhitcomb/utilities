5da974ed3ef029e07ba6cc2343e03ce3d180b3b6
rlwhitcomb <roger@whitcomb.info>
2022-01-20 12:47:53 -0800

    Simplify "latest_log" even more.

--- java/latest_log.bat
+++ java/latest_log.bat
@@ -1,2 +1,2 @@
 @echo off
-for /f "usebackq" %%F in (`call c -noc -res -- test*.log -- "max(sort($*)) @q"`) do notepad %%F
+for /f "usebackq" %%F in (`call c -noc -res -- test*.log -- "max($*) @q"`) do notepad %%F
