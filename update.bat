bac48946d4434ea6d02c0f313d48d6d5a327d9d1
Roger L. Whitcomb <rlwhitcomb@users.noreply.github.com>
2022-01-31 10:58:34 -0800

    Update "update.bat" to do everything in one shot

--- update.bat
+++ update.bat
@@ -1,8 +1,19 @@
 @echo off
+setlocal
+::
+:: Parameter is the new revision tag to use
+::
+if "%1" == "" goto usage
+set COMMIT=%1
 call ant unzip
 if errorlevel 1 exit /b %errorlevel%
 cd utilities-master\java
-echo Run "ant fixup -Dcommit=xxxxxxx" where "xxxxxxx" is the latest git revision,
-echo then run "ant all-install" to finish the installation.
-echo.
+echo Fixing up the latest revision tag...
+call ant fixup -Dcommit=%COMMIT%
+echo Doing full build with latest code...
+call ant all-install
+echo Done.
+exit /b 0
 
+:usage
+echo Usage: %0 _latest_commit_tag_
