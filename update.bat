c1dbac0ee35e7705d87d310627a3753854b7262d
rlwhitcomb <roger@whitcomb.info>
2022-05-23 07:52:18 -0700

    #164: Enhance the "update.bat" process to not require any manual input.

--- update.bat
+++ update.bat
@@ -1,17 +1,9 @@
 @echo off
-::
-:: Parameter is the new revision tag to use
-::
-if "%1" == "" goto usage
-call ant unzip
+call ant -f update.xml
 if errorlevel 1 exit /b %errorlevel%
 cd utilities-master\java
-echo Fixing up the latest revision tag...
-call ant fixup -Dcommit=%1
 echo Doing full build with latest code...
 call ant all-install
+if errorlevel 1 exit /b %errorlevel%
 echo Done.
 exit /b 0
-
-:usage
-echo Usage: %0 _latest_commit_tag_
