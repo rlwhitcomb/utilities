0dc1af1492f0a856f19b48f18ffac5b945618565
rlwhitcomb <roger@whitcomb.info>
2022-07-07 23:40:23 -0700

    #391: Modify the "update.bat" process for Windows to override the commit id if necessary.

--- update.bat
+++ update.bat
@@ -1,5 +1,9 @@
 @echo off
-call ant -f update.xml
+if "%1" == "-?" goto :help
+if "%1" == "-help" goto :help
+if "%1" == "?" goto :help
+
+call ant -f update.xml %*
 if errorlevel 1 exit /b %errorlevel%
 cd utilities-master\java
 echo Doing full build with latest code...
@@ -7,3 +11,10 @@ call ant all-install
 if errorlevel 1 exit /b %errorlevel%
 echo Done.
 exit /b 0
+
+:help
+echo.
+echo Usage: %~n0 ( -Dcommit=xxxxxxx )
+echo.
+echo ^ ^ Downloads the latest source .zip from github, unzip, and do the complete build.
+echo.
