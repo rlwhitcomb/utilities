ab1e0382dc99641eec6e63aeaebe8149d8331e43
Roger L. Whitcomb <rlwhitcomb@users.noreply.github.com>
2022-07-21 08:57:57 -0800

    #421: Update Java 8 build files to work with either Linux or MacOS

--- ci-build8-first.sh
+++ ci-build8-first.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 . ci-setenv
 echo Start of "ci-build8-first.sh", current directory: `pwd`
-cd /home/runner/work/utilities/utilities/java
+cd java
 ant -DCI_BUILD=true clean process-grammars
