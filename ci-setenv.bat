3864bc35ac571e2beb4d2639fe302f283efee933
rlwhitcomb <roger@whitcomb.info>
2022-07-21 19:27:56 -0700

    #423: Fix syntax errors in Windows "ci-setenv".

--- ci-setenv.bat
+++ ci-setenv.bat
@@ -1,10 +1,10 @@
 @echo off
 set PROJECT_HOME=%CD%
 set UTILITIES_HOME=%PROJECT_HOME%\dist
-if not exist %UTILITIES_HOME% do mkdir %UTILITIES_HOME%
+if not exist %UTILITIES_HOME% mkdir %UTILITIES_HOME%
 set PATH=%PATH%;%UTILITIES_HOME%
 
-# Setup the CLASSPATH with everything we will need
+:: Setup the CLASSPATH with everything we will need
 set CLASSPATH=.;%PROJECT_HOME%\java
-for /F %%J in (%PROJECT_HOME%\java\external-files\*.jar) do set CLASSPATH=%CLASSPATH%;%%J
+for %%J in (%PROJECT_HOME%\java\external-files\*.jar) do set CLASSPATH=%CLASSPATH%;%%J
 
