f3860f2f1e9ebf04aca488e9384572351154d648
rlwhitcomb <roger@whitcomb.info>
2022-07-24 15:25:28 -0700

    #423: Put our utilities directory first in the path to avoid conflicts with "cmp" command in Windows. Take out debugging code in "ci-setenv.bat".

--- ci-setenv.bat
+++ ci-setenv.bat
@@ -2,13 +2,11 @@
 set PROJECT_HOME=%CD%
 set UTILITIES_HOME=%PROJECT_HOME%\dist
 if not exist %UTILITIES_HOME% mkdir %UTILITIES_HOME%
-set PATH=%PATH%;%UTILITIES_HOME%
+set PATH=%UTILITIES_HOME%;%PATH%
 
 :: Setup the CLASSPATH with everything we will need
 set CLASSPATH=.;%PROJECT_HOME%\java
-dir %PROJECT_HOME%\java\external-files\*.jar
 for %%J in (%PROJECT_HOME%\java\external-files\*.jar) do call :addclass %%J
-echo CLASSPATH=%CLASSPATH%
 exit /b 0
 
 :addclass
