e6d603e471ab1dd0565152d9bffbd4ca8335dca4
Roger L. Whitcomb <rlwhitcomb@users.noreply.github.com>
2022-10-06 08:24:59 -0800

    #497: Update Calc output flags so verify_calc2 has correct charset

--- java/verify_calc2.bat
+++ java/verify_calc2.bat
@@ -2,7 +2,7 @@
 :: Verify the calculation of Mersenne number M13699727 divisible by 3067752467657
 set NUMBER=13699727
 if exist test\data\M%NUMBER%.txt.gz call gunz -keep -out m.txt test\data\M%NUMBER%.txt.gz
-call c test\files\m.calc >m2.txt
+call c test\files\m.calc -outputcharset UTF-8 -output m2.txt
 call cmp -c -b -q --ignore-line-endings m.txt m2.txt
 if errorlevel 1 (
    echo Differences found!
