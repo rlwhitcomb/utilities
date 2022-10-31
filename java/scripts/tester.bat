6f1a3e7e1ab8f8e1696e49aecd5bfb6acc59480c
rlwhitcomb <roger@whitcomb.info>
2022-10-30 23:42:20 -0700

    #541: Only set the JVM17 arguments in the wrapper files for Java 17+.

--- java/scripts/tester.bat
+++ java/scripts/tester.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 set CLASS_NAME=info.rlwhitcomb.tester.Tester
-set JVM_ARGS=-Djava.security.manager=allow
+set JVM_ARGS=@@@JVM17+@@@
 call %~dp0_find_and_run_class %*
 endlocal
 exit /b %errorlevel%
