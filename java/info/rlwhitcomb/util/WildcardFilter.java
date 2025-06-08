82987100d2474c586192d4c6ef545f9c0abae4a5
rlwhitcomb <roger@whitcomb.info>
2025-06-07 21:51:46 -0700

    #723: Allow multiple file specs in "findfiles" function.

--- java/info/rlwhitcomb/util/WildcardFilter.java
+++ java/info/rlwhitcomb/util/WildcardFilter.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2022 Roger L. Whitcomb.
+ * Copyright (c) 2022,2025 Roger L. Whitcomb.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,9 +24,10 @@
  *	Both a FileFilter and a FilenameFilter based on a wildcard pattern.
  *
  * History:
- *  20-Sep-22 rlw #448: Initial coding.
- *  21-Oct-22 rlw #473: Add flags value to the mix.
- *  24-Oct-22 rlw #473: Tiny bit of refactoring. Add "ignoreCase" flag.
+ *  20-Sep-22 rlw #448	Initial coding.
+ *  21-Oct-22 rlw #473	Add flags value to the mix.
+ *  24-Oct-22 rlw #473	Tiny bit of refactoring. Add "ignoreCase" flag.
+ *  07-Jun-25 rlw #723	Rework to use multiple specs for the file name.
  */
 package info.rlwhitcomb.util;
 
@@ -36,6 +37,7 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.FilenameFilter;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 
 /**
@@ -48,74 +50,96 @@ import java.util.Optional;
 public class WildcardFilter implements FileFilter, FilenameFilter
 {
 	/**
-	 * The wildcard spec for filtering.
+	 * The wildcard spec(s) for filtering.
 	 */
-	private String wildcardSpec;
+	private String[] wildcardSpecs;
+
 	/**
-	 * Whether this pattern actually has any wildcard characters.
+	 * Whether any of the patterns actually have any wildcard characters.
 	 */
 	private boolean anyWild;
+
 	/**
 	 * The flags ({@code "d", "dr", "f", "fr", "fw", "fx"}).
 	 */
 	private Optional<String> flags;
+
 	/**
 	 * Whether to ignore case when matching names.
 	 */
 	private boolean ignoreCase;
 
+	/**
+	 * Pattern to use in separating multiple wildcard specs.
+	 */
+	private static final Pattern MULTI_SPECS = Pattern.compile("\\s*[;:]\\s*");
+
 
 	/**
-	 * Construct using the given wildcard file specification, and case-sensitive
+	 * Construct using the given wildcard file specification(s), and case-sensitive
 	 * name comparison.
 	 *
-	 * @param spec	The wildcard pattern of files (or directories) to accept.
+	 * @param specs	The wildcard pattern(s) of files (or directories) to accept.
 	 */
-	public WildcardFilter(final String spec) {
-	    this(spec, null, false);
+	public WildcardFilter(final String specs) {
+	    this(specs, null, false);
 	}
 
 	/**
-	 * Construct using the given wildcard file specification, as well as
+	 * Construct using the given wildcard file specification(s), as well as
 	 * flags value, and case-sensitive comparison.
 	 *
-	 * @param spec	A wildcard file specification, used to match the file name.
+	 * @param specs	The wildcard file specification(s), used to match the file name.
 	 * @param flag	One of a possible set of flags to further qualify the file
 	 *		to be matched by name.
 	 */
-	public WildcardFilter(final String spec, final String flag) {
-	    this(spec, flag, false);
+	public WildcardFilter(final String specs, final String flag) {
+	    this(specs, flag, false);
 	}
 
 	/**
-	 * Construct using the given wildcard file specification, as well as
+	 * Construct using the given wildcard file specification(s), as well as
 	 * flags value, with option to ignore case of names.
 	 *
-	 * @param spec		A wildcard file specification, used to match the file name.
+	 * @param specs		Wildcard file specification(s), used to match the file name.
 	 * @param flag		One of a possible set of flags to further qualify the file
 	 *			to be matched by name.
 	 * @param ignore	Whether to ignore name case when matching.
 	 */
-	public WildcardFilter(final String spec, final String flag, final boolean ignore) {
-	    wildcardSpec = spec;
-	    anyWild = Match.hasWildCards(wildcardSpec);
+	public WildcardFilter(final String specs, final String flag, final boolean ignore) {
+	    wildcardSpecs = MULTI_SPECS.split(specs);
+	    anyWild = false;
+	    for (String spec : wildcardSpecs) {
+		if (Match.hasWildCards(spec)) {
+		    anyWild = true;
+		    break;
+		}
+	    }
 	    flags = Optional.ofNullable(flag);
 	    ignoreCase = ignore;
 	}
 
 	/**
-	 * Does the given name match the pattern?
+	 * Does the given name match one of the patterns?
 	 *
 	 * @param file	File to match (by name and flags).
-	 * @return	Whether the name matches the pattern, and flags (if any).
+	 * @return	Whether the name matches any pattern, and flags (if any).
 	 */
 	private boolean matches(final File file) {
-	    boolean matched;
+	    boolean matched = false;
 	    String name = file.getName();
-	    if (anyWild) {
-		matched = Match.stringMatch(name, wildcardSpec, !ignoreCase);
-	    } else {
-		matched = ignoreCase ? name.equalsIgnoreCase(wildcardSpec) : name.equals(wildcardSpec);
+	    for (String spec : wildcardSpecs) {
+		// Note: "anyWild" is set if ANY of the specs is wild, without noting which one is
+		// BUT it won't hurt to use the wildcard matching for a non-wild spec; it will just
+		// take a bit longer.
+		if (anyWild) {
+		    matched = Match.stringMatch(name, spec, !ignoreCase);
+		} else {
+		    matched = ignoreCase ? name.equalsIgnoreCase(spec) : name.equals(spec);
+		}
+		if (matched) {
+		    break;
+		}
 	    }
 	    if (matched && flags.isPresent()) {
 		matched = FileUtilities.exists(file, flags.get());
